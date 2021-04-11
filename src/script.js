5e431330c64c26f498900ef7934ff7dfdf2094b0
Umair Arshad <Umair6622@gmail.com>
2021-04-11 14:04:47 +0500

    converting to boolean

---
+++ src/script.js
@@ -0,0 +1,9 @@
+// Converting to Boolean
+console.log("Converting to Boolean")
+const isTrue = !0;
+const isFalse = !1; //isFalse=!!0
+
+console.log("The result will be true: "+isTrue);
+console.log("Type of true is: ", typeof true);
+
+
